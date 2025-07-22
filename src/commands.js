3aaea55c89b58ec53869c795dad0582ea0423eee
Jack Dodd <jack.dodd@dxtra.io>
2025-07-22 18:14:03 +1200

    feat: add options for URL input in add command for playlist

--- src/commands.js
+++ src/commands.js
@@ -6,4 +6,12 @@
 export const ADD_COMMAND = {
   name: 'add',
   description: 'Add a new song to the playlist',
+  options: [
+    {
+      name: 'url',
+      description: 'The URL of the song to add',
+      type: 3, // STRING
+      required: true,
+    },
+  ],
 };
